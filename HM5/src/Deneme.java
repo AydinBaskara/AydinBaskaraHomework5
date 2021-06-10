623b58124d275b815aa9e84478d8b3ced2d15598
AydinBaskara <19050151002@ybu.edu.tr>
2021-06-10 21:52:55 +0300

    Added for loop

--- HM5/src/Deneme.java
+++ HM5/src/Deneme.java
@@ -4,6 +4,11 @@ public class Deneme {
 	public static void main(String[] args) {
 		System.out.println("This is first git project.");
 		System.out.println("I did some modification");
+		
+		//for loop
+		for(int i = 0; i < 10; i++) {
+		   System.out.println("Deger: " + i);
+		}
 	}
 
 }
